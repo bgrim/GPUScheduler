62376f87523bc6de9f6a3db82046fe682480ef22
Scott Krieder <skrieder@iit.edu>
2012-08-02 11:05:05 -0500

    created mat sq

---
+++ Kernels/MatrixSquare.cu
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+__device__ void addSleep(void *p_us_time)
+{ 
+    //This method will sleep for clockRate*kernel_time many clock ticks
+    // which is equivalent to sleeping for kernel_time milliseconds
+    int time = *((int *) p_us_time);
+
+//    float AddPerUs = 10.26188; //Ben
+    float AddPerUs = 9.89996; //Scott
+
+    float adds = time*AddPerUs;
+
+    int temp=0;
+    while(temp<adds){
+       temp++;
+    }
+}
