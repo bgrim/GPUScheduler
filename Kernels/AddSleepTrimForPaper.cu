02757c02946ffc7a5d9f9ca0644a1f1f1be5a8eb
Scott J. Krieder <skrieder@iit.edu>
2012-10-01 11:13:38 -0500

    Update Kernels/AddSleepTrimForPaper.cu

--- Kernels/AddSleepTrimForPaper.cu
+++ Kernels/AddSleepTrimForPaper.cu
@@ -3,9 +3,10 @@
 __device__ void addSleep(void *p_us_time)
 { 
     int time = *((int *) p_us_time);
-
-    float AddPerUs = 9.89759943623274; // GTX-670
-    
+ 
+    // GTX-670
+    float AddPerUs = 9.89759943623274; 
+ 
     float adds = time*AddPerUs;
 
     int temp=0;
